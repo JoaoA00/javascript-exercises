e784fdfba3672566b41470e6709ca2344ceaf8aa
João Abner <joaoabner2022@gmail.com>
2024-10-17 18:35:16 -0300

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempfahrenheit) {
+  const celsius = Math.round((tempfahrenheit - 32) * 5/9 * 10) / 10; //*10 /10 foram pra deixar com 1 casa decimal
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const fahrenheit = Math.round(((tempCelsius * 9/5) + 32)*10) / 10; //*10 /10 foram pra deixar com 1 casa decimal
+  return fahrenheit;
 };
 
 // Do not edit below this line
