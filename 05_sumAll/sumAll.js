47d0ffa0a01d832721cdde76788e219d43b939a1
João Abner <joaoabner2022@gmail.com>
2024-10-17 13:25:17 -0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else if (num2 > num1){
+        result = (num1+num2) * (num2 - num1 + 1)/2;
+    } else {
+        result = (num1+num2) * (num1 - num2 + 1)/2;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
