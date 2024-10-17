082e0eb1349777224439e1b383238626b3b01520
João Abner <joaoabner2022@gmail.com>
2024-10-17 18:17:54 -0300

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    const divQuatro = year % 4 == 0;
+    const divCem = year % 100 == 0;
+    const divQuatrocentos = year % 400 == 0;
+
+    if ((divCem && divQuatrocentos) || (divQuatro && !divCem))  {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
