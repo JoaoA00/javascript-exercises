027c959831322064b93ecf655cd091535d82bead
João Abner <joaoabner2022@gmail.com>
2024-11-22 13:17:18 -0300

    solve palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    return cleanString === cleanString.split('').reverse().join('');
 };
 
 // Do not edit below this line
