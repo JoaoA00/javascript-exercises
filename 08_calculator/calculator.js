9025ae0ba7ecd3caf6f19f2805139ac723b1dd41
João Abner <joaoabner2022@gmail.com>
2024-11-22 11:00:15 -0300

    Solve calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  return a.reduce((total, a) => total + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total, a) => total*a, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let totFactorial = 1;
+  
+  if(a != 0){
+    do {
+      totFactorial *= a;
+      a--;
+    } while (a >= 1);
+  }
+
+	return totFactorial;
 };
 
 // Do not edit below this line
