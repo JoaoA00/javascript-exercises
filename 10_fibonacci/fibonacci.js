30c13bf6c61767e55f7f44507cc8251009149340
João Abner <joaoabner2022@gmail.com>
2024-11-23 13:33:04 -0300

    solve fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let numAnterior = 0, 
+        numAtual = 1, 
+        manterNumAnterior = 0,
+        position = n - 1;
 
+    if(n > 0){
+        for(let i = 0; i < position; i++){
+            manterNumAnterior = numAnterior;
+            numAnterior = numAtual;
+            numAtual += manterNumAnterior;
+        }
+    } else if (n == 0){
+        return 0;
+    } else {
+        return 'OOPS';
+    }
+
+    return numAtual;
 };
 
 // Do not edit below this line
