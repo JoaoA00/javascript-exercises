dd7577bc50ce37f2a9f3a9c6a546882a5ffef0e2
João Abner <joaoabner2022@gmail.com>
2024-11-23 14:13:20 -0300

    Solve findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestToYougest = people.sort((a, b) => {
+        if(a.yearOfDeath == undefined) a.yearOfDeath = new Date().getFullYear();
+        if(b.yearOfDeath == undefined) b.yearOfDeath = new Date().getFullYear();
 
+        return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+    });
+    return oldestToYougest[0];
 };
 
 // Do not edit below this line
